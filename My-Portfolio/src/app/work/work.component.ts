4bab223aa9013c4e3d85a68e1c04d709e4dcd1fa
ehsan <ehsan02032014@gmail.com>
2024-09-01 12:15:49 +0330

    Fix Seo

--- My-Portfolio/src/app/work/work.component.ts
+++ My-Portfolio/src/app/work/work.component.ts
@@ -12,9 +12,9 @@ export class WorkComponent {
   constructor(private titleService: Title, private metaService: Meta) { }
 
   ngOnInit(): void {
-    this.titleService.setTitle('خدمات احسان چیت‌ساز | طراحی وب، برنامه‌نویسی و توسعه سایت');
+    this.titleService.setTitle('خدمات طراحی و توسعه وب توسط احسان چیت‌ساز - بهینه‌سازی سئو و تجربه کاربری');
     this.metaService.updateTag({ name: 'description', content: 'احسان چیت‌ساز، طراح و برنامه‌نویس وب حرفه‌ای، ارائه دهنده خدمات طراحی صفحات شیک، توسعه سایت‌های واکنش‌گرا، و برنامه‌نویسی اپلیکیشن‌های مدرن با استفاده از HTML, CSS, JavaScript, ASP.NET و Blazor , Angular.' });
     this.metaService.updateTag({ name: 'keywords', content: 'خدمات طراحی وب, برنامه‌نویسی سایت, توسعه سایت‌های واکنش‌گرا, HTML, CSS, JavaScript, ASP.NET, Blazor, احسان چیت‌ساز' });
-    this.metaService.updateTag({ property: 'og:title', content: 'خدمات احسان چیت‌ساز | طراحی وب، برنامه‌نویسی و توسعه سایت' });
+    this.metaService.updateTag({ property: 'og:title', content: 'خدمات طراحی و توسعه وب توسط احسان چیت‌ساز - بهینه‌سازی سئو و تجربه کاربری' });
     this.metaService.updateTag({ property: 'og:description', content: 'با خدمات حرفه‌ای احسان چیت‌ساز، طراحی و توسعه سایت‌های مدرن و اپلیکیشن‌های قدرتمند را تجربه کنید.' });  }
 }
